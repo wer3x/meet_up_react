d94f3251fd16543609fbcedf5a2040728bf316a8
Eugenii <wer3xxx@gmail.com>
2019-12-14 21:14:29 +0300

    Create countdown-timer-ceil.tsx

---
+++ src/components/countdown-timer/countdown-timer-ceil.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export interface IProps {
+    value: number;
+    description: string;
+}
+
+const NotesControl: React.FC<IProps> =
+    (props: IProps) => {
+        return (
+            <div className="timer__ceil">
+                <span className="timer__ceil-value">
+                {props.value}
+                </span>
+                <span className="timer__ceil-value">
+                {props.description}
+                </span>
+            </div>
+        );
+    };
+
+export default NotesControl;
